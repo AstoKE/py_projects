ec593c57234b7951b899e4aa8a9079ebd832a855
AstoK <astokrappersteam@gmail.com>
2025-09-02 19:20:31 +0300

    Finished
    Last converting image to pdf function codes are added and finished program.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import filedialog
+from reportlab.pdfgen import canvas
+from PIL import Image
 import os
 
 class ImageToPDFConverter:
     def __init__(self, root):
         self.root = root
-        self.image_path = []
+        self.image_paths = []
         self.output_pdf_name = tk.StringVar()
         self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 
@@ -28,6 +30,43 @@ class ImageToPDFConverter:
 
         convert_button = tk.Button(self.root, text="Convert to PDF", command=self.convert_images_to_pdf)
         convert_button.pack(pady=(20, 40))
+
+    def select_images(self):
+        self.image_paths = filedialog.askopenfilenames(title="Select Images", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+        self.update_selected_images_listbox()
+
+    def update_selected_images_listbox(self):
+        self.selected_images_listbox.delete(0, tk.END)
+
+        for image_path in self.image_paths:
+            _, image_path = os.path.split(image_path)
+            self.selected_images_listbox.insert(tk.END, image_path)
+
+    def convert_images_to_pdf(self):
+        if not self.image_paths:
+            return
+
+        output_pdf_path = self.output_pdf_name.get() + ".pdf" if self.output_pdf_name.get() else "output.pdf"
+
+        pdf = canvas.Canvas(output_pdf_path, pagesize=(612, 792))
+
+        for image_path in self.image_paths:
+            img = Image.open(image_path)
+            avaliable_width = 540
+            avaliable_height = 720
+            scale_factor = min(avaliable_width / avaliable_height, avaliable_height / avaliable_width)
+            new_width = img.width * scale_factor
+            new_height = img.height * scale_factor
+            x_center = (612 - new_width) / 2
+            y_center = (792 - new_height) / 2
+
+            pdf.setFillColorRGB(255, 255, 255)
+            pdf.rect(0, 0, 612, 792, fill=True)
+            pdf.drawInlineImage(img, x_center, y_center, width=new_width, height=new_height)
+            pdf.showPage()
+
+        pdf.save()
+
 
 
 def main():
