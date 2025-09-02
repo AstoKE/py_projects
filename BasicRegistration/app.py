9e9f3c91b46031e805e1a0b26dc9dc5022b2bbdc
AstoK <astokrappersteam@gmail.com>
2025-09-02 19:50:43 +0300

    Start of Registration App

---
+++ BasicRegistration/app.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class RegistrationApp(App):
+    def build(self):
+        self.title = "Registartion Form"
+        layout = BoxLayout(orientation='vertical', padding= 30, spacing=10)
+
+        return layout
+    
+if __name__== '__main__':
+    RegistrationApp().run()
+
